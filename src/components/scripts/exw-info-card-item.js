f72344cc8ed3603589edbf1a32ddcfc0525a03ff
liangzhifeng <feng322168@163.com>
2018-03-28 22:52:10 +0800

    卡片

--- src/components/scripts/exw-info-card-item.js
+++ src/components/scripts/exw-info-card-item.js
@@ -24,14 +24,19 @@ export default class extends Component {
             </div>
 
           </div>
-          <div className="info-card-layout ">
-            <div className="info-card-layout-img">
-              <h3 className="tc f14 mb14  b card-text1">
-                  <span>{title}</span>
-              </h3>
-              <h3 className="tc f32 mb14  b">
-                <span>{count}</span>
-              </h3>
+          <div className="info-card-layout-right">
+            <div className="info-card-layout-text">
+              <div className="mb7  b">
+                  <span className="card-text1">{title}</span>
+              </div>
+              <div className="mb7  b">
+                  <span className="card-text2">{count}</span>
+                {apply && <em className="abs c-red wp-10">未读</em>}
+              </div>
+              <div className="mb7  b">
+                <span className="card-text3">{count} 通过 </span>
+                <span className="card-text4">{count} 拒绝 </span>
+              </div>
               {/*<p className="rel">*/}
                   {/*<span className="f48 b">{count}</span>*/}
               {/*{apply && <em className="abs c-red wp-10">待审</em>}*/}
