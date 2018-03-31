3561b39523d7695d6184c0cf0cff96d8b253e509
liangzhifeng <feng322168@163.com>
2018-03-31 18:34:01 +0800

    提交我的报馆详情、施工人员详情、付款通知单列表、汇款水单和发票详情

--- src/admin/builders/index.js
+++ src/admin/builders/index.js
@@ -1,42 +1,85 @@
+import React, {Component} from 'react';
+import {Row, Col, Menu, Icon, Table, Button, Input} from 'antd';
 import AppBase, {
-  $api, $store, $app, $config,
-  AntAbstractControllerIndex
+  $api, $store, $route,
 } from 'components/scripts/index';
-import {Table, Icon, Card, Input, Button, Tabs} from 'antd';
 
-
-@mixin(['tabs-layout'])
-export default class extends AntAbstractControllerIndex {
-  layout = 'tabs';
-
-  get extra() {
-    return (
-      <div className="lfix_ ml10_ ml__ extra">
-        <Input.Search className="dib" style={{width: 220}} enterButton placeholder="do search"/>
-      </div>
-    )
-  }
-
-  get route() {
-    return {
-      path: '/admin/builders/index/:state',
-      component: require('admin/builders/index-status').default
-    };
-  }
-
-  componentDidMount() {
-  }
-
-  get header() {
-    return [
+export default class extends Component {
+  state = {
+    data: [
       {
-        state: '1',
-        component: <span>待审核(12)</span>
+        number: '1232123',
+        location: '展位号',
+        canzhuanshang: '参展商',
+        status: '更新',
+        time: '昨天'
       },
       {
-        state: '2',
-        component: <span>已审核(213)</span>
+        number: '1232123',
+        location: '展位号',
+        canzhuanshang: '参展商',
+        status: '更新',
+        time: '昨天'
+      },
+      {
+        number: '1232123',
+          location: '展位号',
+        canzhuanshang: '参展商',
+        status: '更新',
+        time: '昨天'
       }
-    ];
+    ],
+    columns: [
+      {
+        title: '编号',
+        key: 'number',
+        dataIndex: 'number'
+      },
+      {
+        title: '展位',
+        key: 'location',
+        dataIndex: 'location'
+      },
+      {
+        title: '参展商',
+        key: 'canzhuanshang',
+        dataIndex: 'canzhuanshang'
+      },
+      {
+        title: '状态',
+        key: 'status',
+        dataIndex: 'status'
+      },
+      {
+        title: '通知时间',
+        key: 'time',
+        dataIndex: 'time'
+      },
+      {
+        title: '付款通知单',
+        key: 'action',
+        render: () => {
+          return (
+            <div className="actions">
+              <Button size="small" onClick={$route.push.bind(null, '/admin/builders/show')}>再次下载</Button>
+            </div>
+          )
+        }
+      }
+    ]
+  };
+
+  render() {
+    const {columns, data} = this.state;
+    return (
+      <div className="p20 notification-view">
+        <header className="lrfix_" style={{lineHeight: '32px'}}>
+          <h1 className="b f20">付款通知单</h1>
+          <Input.Search className="right wp-3" placeholder="Search" enterButton/>
+        </header>
+        <div className="blank-20"/>
+        <Table bordered rowKey={'id'} columns={columns} dataSource={data} size="middle"/>
+      </div>
+    )
   }
 }
--- src/admin/worker/show.js
+++ src/admin/worker/show.js
@@ -1,166 +1,176 @@
-import  AppBase, {
-  $api, $route, $modal,
-  AntAbstractControllerIndex
+// import  AppBase, {
+//   $api, $route, $modal,ExwDashboardMyExhibition,
+//   AntAbstractControllerIndex
+// } from 'components/scripts/index';
+//
+// import {Row, Col, Tag, Icon} from 'antd';
+// import ReactSmartPhoto from 'react-smart-photo';
+//
+// @mixin(['pure-layout', 'match'])
+// export default class extends React.Component {
+//
+//   layout = 'pure';
+//
+//   _onClick = inStatus => {
+//     switch (inStatus) {
+//       case 'back':
+//         $route.back();
+//         break;
+//       case 'refuse':
+//         $modal.show('building-businesses-refuse');
+//         break;
+//       default:
+//         AppBase.notify(`You Click status: => ${inStatus}`);
+//     }
+//   };
+//
+//   footerView() {
+//     return (
+//       <div className="lrfix_">
+//         <label className="left" style={{lineHeight: '34px'}}>
+//           <span>搭建商类别：</span>
+//           <select>
+//             <option value="1">搭建商1</option>
+//             <option value="2">搭建商2</option>
+//           </select>
+//         </label>
+//         <div className="right">
+//           <Tag color="#38a0f5" onClick={this._onClick.bind(this, 'back')}>
+//             <Icon type="left"/>
+//             <span>返回</span>
+//           </Tag>
+//           <Tag color="#F34D45" onClick={this._onClick.bind(this, 'refuse')}>拒绝</Tag>
+//           <Tag color="#46A96A" onClick={this._onClick.bind(this, 'pass')}>通过</Tag>
+//         </div>
+//       </div>
+//     );
+//   }
+//
+//   childView() {
+//     return (
+//       <div className="abs trbl0 p20 bg-f my-exhibition-show-view">
+//         <h3 className="mb10">
+//           <a href="javascript:;" onClick={$route.back}>
+//             <Icon type="left" />
+//             <span>返回列表</span>
+//           </a>
+//         </h3>
+//         <h2 className="f20 mb20 b">施工人员详情</h2>
+//         <h3 className="sub-title">
+//           <span className="b f14">有部分施工人员申请被拒绝，请修改并重新提交</span>
+//         </h3>
+//
+//         <ExwDashboardMyExhibition/>
+//       </div>
+//     );
+//   }
+//
+//   componentDidMount() {
+//     AppBase.$.memory = {
+//       footer: this.footerView()
+//     };
+//   }
+//
+//   render() {
+//     return this.pureLayout();
+//   }
+// }
+//
+
+
+import React, {Component} from 'react';
+import {Row, Col, Menu, Icon, Table, Button, Input} from 'antd';
+import AppBase, {
+  $api, $store, $route,
 } from 'components/scripts/index';
-import {Row, Col, Tag, Icon} from 'antd';
-import ReactSmartPhoto from 'react-smart-photo';
 
-@mixin(['pure-layout', 'match'])
-export default class extends React.Component {
+export default class extends Component {
+  state = {
+    endTime:'2018年3月26日18:00',
+    data: [
+      {
+        id: '1',
+        username: '施工人',
+        idcardnumber: '参展商',
+        saomiaojian:'扫描件',
+        yicunzhaopian:'一寸照片',
+      },
+      {
+        id: '2',
+        username: '施工人',
+        idcardnumber: '参展商',
+        saomiaojian:'扫描件',
+        yicunzhaopian:'一寸照片',
+      },
+      {
+        id: '3',
+        username: '施工人',
+        idcardnumber: '参展商',
+        saomiaojian:'扫描件',
+        yicunzhaopian:'一寸照片',
+      },
+      {
+        id: '4',
+        username: '施工人',
+        idcardnumber: '参展商',
+        saomiaojian:'扫描件',
+        yicunzhaopian:'一寸照片',
+      }
 
-  layout = 'pure';
-
-  _onClick = inStatus => {
-    switch (inStatus) {
-      case 'back':
-        $route.back();
-        break;
-      case 'refuse':
-        $modal.show('building-businesses-refuse');
-        break;
-      default:
-        AppBase.notify(`You Click status: => ${inStatus}`);
-    }
+    ],
+    columns: [
+      {
+        title: '姓名',
+        key: 'username',
+        dataIndex: 'username'
+      },
+      {
+        title: '身份证号',
+        key: 'idcardnumber',
+        dataIndex: 'idcardnumber'
+      },
+      {
+        title: '身份证扫描件',
+        key: 'saomiaojian',
+        dataIndex: 'saomiaojian'
+      },
+      {
+        title: '一寸照片',
+        key: 'yicunzhaopian',
+        dataIndex: 'yicunzhaopian'
+      },
+      {
+        title: '',
+        key: 'action',
+        render: () => {
+          return (
+            <div className="actions">
+              <Button size="small" onClick={$route.push.bind(null, '/admin/building-businesses/index/1')}>查看</Button>
+            </div>
+          )
+        }
+      }
+    ]
   };
 
-  footerView() {
-    return (
-      <div className="lrfix_">
-        <label className="left" style={{lineHeight: '34px'}}>
-          <span>搭建商类别：</span>
-          <select>
-            <option value="1">搭建商1</option>
-            <option value="2">搭建商2</option>
-          </select>
-        </label>
-        <div className="right">
-          <Tag color="#38a0f5" onClick={this._onClick.bind(this, 'back')}>
-            <Icon type="left"/>
-            <span>返回</span>
-          </Tag>
-          <Tag color="#F34D45" onClick={this._onClick.bind(this, 'refuse')}>拒绝</Tag>
-          <Tag color="#46A96A" onClick={this._onClick.bind(this, 'pass')}>通过</Tag>
-        </div>
-      </div>
-    );
-  }
-
-  childView() {
-    return (
-      <div className="abs trbl0 p20 bg-f building-businesses-show-view">
-        <dl className="item">
-          <dt className="mb20 b">个人信息 { JSON.stringify(this.params)}</dt>
-          <dd>
-            <ul className="lfix_">
-              <li>
-                <strong>姓名:</strong>
-                <em>afei</em>
-              </li>
-              <li>
-                <strong>职位:</strong>
-                <em className="c-9">未填写</em>
-              </li>
-              <li>
-                <strong>身份证:</strong>
-                <em>123456789012345678</em>
-              </li>
-              <li>
-                <strong>微信号:</strong>
-                <em className="c-9">未填写</em>
-              </li>
-              <li style={{width: '100%'}}>
-                <strong>邮寄地址:</strong>
-                <em className="c-9">未填写</em>
-              </li>
-              <li style={{width: '100%'}}  className="mt10">
-                <strong>身份证扫描件:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="id" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '身份证'
-                    }
-                  ]}/>
-                </em>
-              </li>
-            </ul>
-          </dd>
-        </dl>
-
-        <hr className="my20"/>
-
-        <dl className="item">
-          <dt className="mb20 b">公司信息</dt>
-          <dd>
-            <ul className="lfix_">
-              <li>
-                <strong>公司名称:</strong>
-                <em>公司名称</em>
-              </li>
-              <li>
-                <strong>传真:</strong>
-                <em className="c-9">123-456-7899</em>
-              </li>
-              <li style={{width: '100%'}}>
-                <strong>办公地址:</strong>
-                <em>办公地址</em>
-              </li>
-              <li>
-                <strong>法人姓名:</strong>
-                <em className="c-9">法人姓名</em>
-              </li>
-              <li style={{width: '100%'}}>
-                <strong>法人手机号:</strong>
-                <em className="c-9">12345678901</em>
-              </li>
-              <li>
-                <strong>法人身份证号:</strong>
-                <em className="c-9">123456789012345678</em>
-              </li>
-              <li>
-                <strong>法人座机:</strong>
-                <em className="c-9">未填写</em>
-              </li>
-              <li style={{width: '100%'}}  className="mt10">
-                <strong>法人身份证扫描件:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '法人身份证扫描件'
-                    }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>公司营业执照:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-            </ul>
-          </dd>
-        </dl>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    AppBase.$.memory = {
-      footer: this.footerView()
-    };
-  }
-
   render() {
-    return this.pureLayout();
+    const {endTime,columns, data} = this.state;
+    return (
+      <div className="p20 my-exhibition-view bg-f">
+        <h3 className="mb10">
+                     <a href="javascript:;" onClick={$route.back}>
+                       <Icon type="left" />
+                       <span>返回列表</span>
+                     </a>
+                   </h3>
+        <header>
+          <span className="b f18">施工人员详情</span>
+        </header>
+        <header className="sub-title">
+          <span className="b f14">有部分施工人员申请被拒绝，请修改并重新提交</span>
+        </header>
+        <Table bordered rowKey={'id'} columns={columns} dataSource={data} size="middle"/>
+      </div>
+    )
   }
 }
-
