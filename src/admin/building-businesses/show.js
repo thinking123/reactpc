8836c9523dc786753824eacd06cfddfecf6608c9
liangzhifeng <feng322168@163.com>
2018-04-02 11:26:47 +0800

    提交我的报馆详情Tabs

--- src/admin/building-businesses/show.js
+++ src/admin/building-businesses/show.js
@@ -10,18 +10,22 @@ export default class extends React.Component {
 
   layout = 'pure';
 
-  _onClick = inStatus => {
-    switch (inStatus) {
-      case 'back':
-        $route.back();
-        break;
-      case 'refuse':
-        $modal.show('building-businesses-refuse');
-        break;
-      default:
-        AppBase.notify(`You Click status: => ${inStatus}`);
-    }
+  state = {
+    tabId: '1'
   };
+
+  // _onClick = inStatus => {
+  //   switch (inStatus) {
+  //     case 'back':
+  //       $route.back();
+  //       break;
+  //     case 'refuse':
+  //       $modal.show('building-businesses-refuse');
+  //       break;
+  //     default:
+  //       AppBase.notify(`You Click status: => ${inStatus}`);
+  //   }
+  // };
 
   footerView() {
     return (
@@ -59,188 +63,196 @@ export default class extends React.Component {
           <span className="b f14">有部分材料申请被拒绝，请修改并重新提交</span>
         </h3>
 
-        <dl className="item">
-          <dt className="mb20 b">展位信息 { JSON.stringify(this.params)}</dt>
-          <dd>
-            <ul className="lfix_">
-              <li>
-                <strong>展位:</strong>
-                <em>A103B</em>
-              </li>
-              <li>
-                <strong>参展商:</strong>
-                <em className="c-9">参展商</em>
-              </li>
-              <li>
-                <strong>提交时间:</strong>
-                <em>2017年12月20日</em>
-              </li>
-              </ul>
-            </dd>
-        </dl>
+        <Tabs activeKey={tabId} onChange={this._onChange}>
+          <Tabs.TabPane tab="展位和搭建商信息" key="info">
             <dl className="item">
-          <dt className="mb20 b">搭建商信息</dt>
+              <dt className="mb20 b">展位信息 { JSON.stringify(this.params)}</dt>
               <dd>
-          <ul className="lfix_">
-          <li>
-                <strong>搭建商现场负责人:</strong>
-                <em className="c-9">负责人</em>
-              </li>
-              <li >
-                <strong>现场负责人手机号:</strong>
-                <em className="c-9">12345678909</em>
-              </li>
-              {/*<li style={{width: '100%'}}  className="mt10">*/}
-                {/*<strong>身份证扫描件:</strong>*/}
-                {/*<em className="c-9">*/}
+                <ul className="lfix_">
+                  <li>
+                    <strong>展位:</strong>
+                    <em>A103B</em>
+                  </li>
+                  <li>
+                    <strong>参展商:</strong>
+                    <em className="c-9">参展商</em>
+                  </li>
+                  <li>
+                    <strong>提交时间:</strong>
+                    <em>2017年12月20日</em>
+                  </li>
+                </ul>
+              </dd>
+            </dl>
+            <dl className="item">
+              <dt className="mb20 b">搭建商信息</dt>
+              <dd>
+                <ul className="lfix_">
+                  <li>
+                    <strong>搭建商现场负责人:</strong>
+                    <em className="c-9">负责人</em>
+                  </li>
+                  <li >
+                    <strong>现场负责人手机号:</strong>
+                    <em className="c-9">12345678909</em>
+                  </li>
+                  {/*<li style={{width: '100%'}}  className="mt10">*/}
+                  {/*<strong>身份证扫描件:</strong>*/}
+                  {/*<em className="c-9">*/}
                   {/*<ReactSmartPhoto group="id" items={[*/}
-                    {/*{*/}
-                      {/*href: 'http://placeholder.qiniudn.com/180x180',*/}
-                      {/*src: 'http://placeholder.qiniudn.com/80x80',*/}
-                      {/*caption: '身份证'*/}
-                    {/*}*/}
+                  {/*{*/}
+                  {/*href: 'http://placeholder.qiniudn.com/180x180',*/}
+                  {/*src: 'http://placeholder.qiniudn.com/80x80',*/}
+                  {/*caption: '身份证'*/}
+                  {/*}*/}
                   {/*]}/>*/}
-                {/*</em>*/}
-              {/*</li>*/}
-            </ul>
-          </dd>
-        </dl>
+                  {/*</em>*/}
+                  {/*</li>*/}
+                </ul>
+              </dd>
+            </dl>
 
-        <hr className="my20"/>
+            <hr className="my20"/>
 
-        <dl className="item">
-          <dt className="mb20 b">文件</dt>
-          <dd>
-            <ul className="lfix_">
-              <li style={{width: '100%'}}>
-                <strong>展台类型结构:</strong>
-                <em>室内双层</em>
-              </li>
-              {/*<li>*/}
-                {/*<strong>传真:</strong>*/}
-                {/*<em className="c-9">123-456-7899</em>*/}
-              {/*</li>*/}
-              {/*<li style={{width: '100%'}}>*/}
-                {/*<strong>办公地址:</strong>*/}
-                {/*<em>办公地址</em>*/}
-              {/*</li>*/}
-              {/*<li>*/}
-                {/*<strong>法人姓名:</strong>*/}
-                {/*<em className="c-9">法人姓名</em>*/}
-              {/*</li>*/}
-              {/*<li style={{width: '100%'}}>*/}
-                {/*<strong>法人手机号:</strong>*/}
-                {/*<em className="c-9">12345678901</em>*/}
-              {/*</li>*/}
-              {/*<li>*/}
-                {/*<strong>法人身份证号:</strong>*/}
-                {/*<em className="c-9">123456789012345678</em>*/}
-              {/*</li>*/}
-              {/*<li>*/}
-                {/*<strong>法人座机:</strong>*/}
-                {/*<em className="c-9">未填写</em>*/}
-              {/*</li>*/}
-              <li style={{width: '100%'}}  className="mt10">
-                <strong>展位图纸:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '法人身份证扫描件'
-                    },
-                  {
-                    href: 'http://placeholder.qiniudn.com/180x180',
-                    src: 'http://placeholder.qiniudn.com/80x80',
-                    caption: '法人身份证扫描件'
-                  }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>委托书:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>电工证:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>搭建委托书:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>展商保证书:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>安全责任书:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-              <li style={{width: '100%'}} className="mt10">
-                <strong>押金扣除标准:</strong>
-                <em className="c-9">
-                  <ReactSmartPhoto group="cid" items={[
-                    {
-                      href: 'http://placeholder.qiniudn.com/180x180',
-                      src: 'http://placeholder.qiniudn.com/80x80',
-                      caption: '公司营业执照'
-                    }
-                  ]}/>
-                </em>
-              </li>
-            </ul>
-          </dd>
-        </dl>
+            <dl className="item">
+              <dt className="mb20 b">文件</dt>
+              <dd>
+                <ul className="lfix_">
+                  <li style={{width: '100%'}}>
+                    <strong>展台类型结构:</strong>
+                    <em>室内双层</em>
+                  </li>
+
+                  <li style={{width: '100%'}}  className="mt10">
+                    <strong>展位图纸:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '法人身份证扫描件'
+                        },
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '法人身份证扫描件'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                  <li style={{width: '100%'}} className="mt10">
+                    <strong>委托书:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '公司营业执照'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                  <li style={{width: '100%'}} className="mt10">
+                    <strong>电工证:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '公司营业执照'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                  <li style={{width: '100%'}} className="mt10">
+                    <strong>搭建委托书:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '公司营业执照'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                  <li style={{width: '100%'}} className="mt10">
+                    <strong>展商保证书:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '公司营业执照'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                  <li style={{width: '100%'}} className="mt10">
+                    <strong>安全责任书:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '公司营业执照'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                  <li style={{width: '100%'}} className="mt10">
+                    <strong>押金扣除标准:</strong>
+                    <em className="c-9">
+                      <ReactSmartPhoto group="cid" items={[
+                        {
+                          href: 'http://placeholder.qiniudn.com/180x180',
+                          src: 'http://placeholder.qiniudn.com/80x80',
+                          caption: '公司营业执照'
+                        }
+                      ]}/>
+                    </em>
+                  </li>
+                </ul>
+              </dd>
+            </dl>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="费用清单" key="list">Content of Tab Pane 2</Tabs.TabPane>
+        </Tabs>
+
+
       </div>
     );
   }
 
   componentDidMount() {
-    AppBase.$.memory = {
-      footer: this.footerView()
-    };
+    this.update('info');
   }
+
+  update(inTabId) {
+    AppBase.$.memory = {tabId: inTabId,};
+    AppBase.$.memory = {footer: this.footerView()};
+  }
+
+
+  _onClick = inStatus => {
+    switch (inStatus) {
+      case 'back':
+        $route.back();
+        break;
+      case 'prev':
+        this.update('info');
+        break;
+      case 'next':
+        this.update('list');
+        break;
+      default:
+        AppBase.notify(`You Click status: => ${inStatus}`);
+    }
+  };
+
+  _onChange = inTabId => {
+    this.update(inTabId);
+  };
 
   render() {
     return this.pureLayout();
