2b5fac1ca6c9ba4cb7e35f3656404251e70a5c26
liangzhifeng <feng322168@163.com>
2018-04-07 10:59:30 +0800

    新增费用水单 优化

--- src/admin/my-exhibition/add.js
+++ src/admin/my-exhibition/add.js
@@ -403,9 +403,246 @@ export default class extends Component {
                             </Row>
                             <Button className="text-button" icon="plus">添加</Button>
                           </div>
-                          <div className="table-like">
 
+
+
+
+
+
+
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={8}>
+                                <strong>用水、气服务</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">单价</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">数量</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={8}>
+                                <Select defaultValue="1" style={{width: 200}}>
+                                  <Option value="1">用电</Option>
+                                </Select>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <InputNumber min={1} defaultValue={1}/>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                            <Button className="text-button" icon="plus">添加</Button>
                           </div>
+
+
+
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={8}>
+                                <strong>有限网络</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">单价</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">数量</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={8}>
+                                <Select defaultValue="1" style={{width: 200}}>
+                                  <Option value="1">用电</Option>
+                                </Select>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <InputNumber min={1} defaultValue={1}/>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                            <Button className="text-button" icon="plus">添加</Button>
+                          </div>
+
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={12}>
+                                <strong>施工证</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">数量</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={12}>
+                                <em className="c-9">辆/限3小时，超过时间按照200元/小时收延时费。<br />（车证押金为600元/张，未退回将扣除全部车证押金）</em>
+                              </Col>
+
+                              <Col span={4}>
+                                <InputNumber min={1} defaultValue={1}/>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                          </div>
+
+
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={12}>
+                                <strong>车证</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">数量</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={12}>
+                                <em className="c-9">辆/限 2 小时，每张车证限定 1 辆货车牌照号</em>
+                              </Col>
+                              <Col span={4}>
+                                <InputNumber min={1} defaultValue={1}/>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                          </div>
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={12}>
+                                <strong>安全员袖章</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">数量</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={12}>
+                                <em className="c-9">100㎡以下（含），1名安全员<br />
+                                  安全员展会期间必须在展位，负责展位安全，监督施工<br />人员安全作业，配合主场管理。</em>
+                              </Col>
+                              <Col span={4}>
+                                <InputNumber min={1} defaultValue={1}/>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                          </div>
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={16}>
+                                <strong>施工管理费</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={16}>
+                                <em className="c-9">按展位实际面积计算</em>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                          </div>
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={16}>
+                                <strong>施工押金</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={16}>
+                                <em className="c-9">按展位实际面积计算</em>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                          </div>
+
+                          <div className="table-like">
+                            <Row>
+                              <Col span={16}>
+                                <strong>滞纳金</strong>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">金额</strong>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col span={16}>
+                                <em className="c-9">当前未超过截止时间，不需要缴纳</em>
+                              </Col>
+                              <Col span={4}>
+                                <strong className="c-9">121</strong>
+                              </Col>
+                              <Col span={4}>
+                                <Icon type="close"/>
+                              </Col>
+                            </Row>
+                          </div>
+
                         </div>
                       </li>
                     </ul>
